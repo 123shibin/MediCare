8edb7aa6269f98fc839a07c24232f083a54ff56a
SHIBIN G <shibingilbert104@gmail.com>
2026-04-26 08:09:44 -0700

    refactor: restructure main component and routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Routes, Route, Navigate } from "react-router-dom";
+import ProtectedRoute from "./routes/ProtectedRoute.jsx";
 import Login from "./pages/login.jsx";
+import Dashboard from "./pages/AdminDashboard.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />  
+        <Route path="/" element={<Navigate to="/login" replace />} /> 
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/dashboard" element={<Dashboard />} />  */}
+        
+        {/* Add more routes here */}
+        <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
       </Routes>
-    </BrowserRouter>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import { QueryClientProvider } from "@tanstack/react-query";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "./app/store";
+import { queryClient } from "./utils/queryClient";
+
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    {/* ✅ Redux Persist */}
+    <PersistGate loading={null} persistor={persistor}>
+      
+      {/* ✅ React Query */}
+      <QueryClientProvider client={queryClient}>
+        
+        {/* ✅ React Router */}
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+
+      </QueryClientProvider>
+
+    </PersistGate>
+  </Provider>
+);
