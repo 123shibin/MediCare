6e42fabb7caaeb60034f181515a19530d4e4a84e
123shibin <shibingilbert104@gmail.com>
2026-09-17 08:12:53 +0530

    feat: Implement JWT authentication with JwtUtil and JwtAuthFilter classes

---
+++ backend/src/main/java/com/medicare/backend/security/JwtAuthFilter.java
@@ -0,0 +1,62 @@
+package com.medicare.backend.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import com.medicare.backend.service.CustomUserDetailsService;
+
+
+import java.io.IOException;
+
+@RequiredArgsConstructor 
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    
+    private final JwtUtil jwtUtil;
+
+    
+    private final CustomUserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        FilterChain chain
+    ) throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+
+            String token = header.substring(7);
+
+            if (jwtUtil.validateToken(token)) {
+
+                String email = jwtUtil.extractEmail(token);
+                UserDetails userDetails =
+                    userDetailsService.loadUserByUsername(email);
+
+                UsernamePasswordAuthenticationToken auth =
+                    new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                    );
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+}
+
---
+++ backend/src/main/java/com/medicare/backend/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.medicare.backend.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+
+import com.medicare.backend.models.User;
+import com.medicare.backend.repository.UserRepository;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+    	System.out.println("🔥 loadUserByUsername CALLED with: " + email);
+        User user = repo.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new CustomUserDetail(user);
+    }
+}
