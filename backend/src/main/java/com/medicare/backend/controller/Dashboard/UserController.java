c8a035b0cca9ec313d27b2eb062840fa3046857b
123shibin <shibingilbert104@gmail.com>
2026-09-24 13:09:57 +0530

    feat: implement addUser endpoint in UserController

--- backend/src/main/java/com/medicare/backend/controller/Dashboard/UserController.java
+++ backend/src/main/java/com/medicare/backend/controller/Dashboard/UserController.java
@@ -1,20 +1,31 @@
 package com.medicare.backend.controller.Dashboard;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.medicare.backend.dto.userdto.AddUserRequest;
+import com.medicare.backend.models.dashboard.User;
+import com.medicare.backend.service.Dashboard.UserService;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-
+@RequiredArgsConstructor 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    public final UserService userService;
     @PostMapping("/adduser")
-    public ResponseEntity<?> addUser(@RequestBody String entity) {
-        //TODO: process POST request
-        
-        return ResponseEntity.ok(entity);
+    public ResponseEntity<?> addUser(@RequestBody AddUserRequest request) {
+        User createdUser = userService.addUser(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(createdUser);
     }
     
 }
